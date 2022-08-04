6f3c775fdd1569a886d263ca45588d11ea94346c
Светлана Ременюк <lan2828@yandex.ru>
2022-08-04 10:54:08 +0300

    upd

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -93,13 +93,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
-
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    ],
 }
 
 SIMPLE_JWT = {
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=3),
-   'AUTH_HEADER_TYPES': ('Bearer',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
+    'AUTH_HEADER_TYPES': ('Bearer', 'Token'),
 }
