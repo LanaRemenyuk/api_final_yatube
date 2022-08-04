edef1285aca999f378859b2e3e1822db5b2a75a3
Светлана Ременюк <lan2828@yandex.ru>
2022-08-04 11:07:45 +0300

    updated

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -19,6 +19,13 @@ class Post(models.Model):
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts')
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='posts',
+    )
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
 
